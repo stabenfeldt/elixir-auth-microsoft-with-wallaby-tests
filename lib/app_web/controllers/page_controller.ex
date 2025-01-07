22a95f3cdea40b2341037e36fbf76c6feacdcc52
Martin Stabenfeldt <martin@stabenfeldt.net>
2025-01-07 11:02:48 +0100

    add index

--- lib/app_web/controllers/page_controller.ex
+++ lib/app_web/controllers/page_controller.ex
@@ -6,4 +6,11 @@ defmodule AppWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def index(conn, _params) do
+    state = "random_state_uid"
+
+    oauth_microsoft_url = ElixirAuthMicrosoft.generate_oauth_url_authorize(conn, state)
+    render(conn, :index, oauth_microsoft_url: oauth_microsoft_url)
+  end
 end
