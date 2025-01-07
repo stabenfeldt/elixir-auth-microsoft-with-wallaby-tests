9feca835d25c8992876f95364244f5313b3b1cf2
Martin Stabenfeldt <martin@stabenfeldt.net>
2025-01-07 09:38:36 +0100

    add elixir_auth_microsoft

--- mix.exs
+++ mix.exs
@@ -54,7 +54,8 @@ defmodule App.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:elixir_auth_microsoft, "~> 1.0.0"}
     ]
   end
 
