264daaa536b2c9414444cd61ee3f03f817d25d41
Martin Stabenfeldt <martin@stabenfeldt.net>
2025-01-07 13:23:38 +0100

    test as logged in and visitor

--- test/app_web/features/frontpage.exs
+++ test/app_web/features/frontpage.exs
@@ -1,12 +1,33 @@
 defmodule AppWeb.FrontPageFeatureTest do
-  use ExUnit.Case, async: true
+  use AppWeb.ConnCase
   use Wallaby.Feature
   import Wallaby.Query
 
 
-  feature "should render as expected", %{session: session} do
+  feature "Visit the frontpage as a visitor", %{session: session} do
     session
     |> visit("/")
     |> assert_has(css(".phx-hero", text: "To get started, login to your Microsoft Account"))
   end
+
+  feature "Visit the frontpage as a logged in user", %{session: session} do
+    session
+    |> visit("/")
+    |> assert_has(css(".phx-hero", text: "Welcome John Doe"))
+  end
+  
+  defp visit_a_page_to_set_auth_cookie_for_wallaby(session, conn) do
+
+    setup context do
+      log_in(context)
+    end
+
+    session
+    |> visit("/")
+    |> set_cookie(
+      @admin_cookie,
+      conn.resp_cookies[@admin_cookie][:value]
+    )
+  end
+
 end
